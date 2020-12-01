40a1c82529cf6a3385c4952397c8b67dcd76b9fc
tplane-js <jsdev.remote@gmail.com>
2020-12-01 00:28:28 -0500

    small fixes

--- src/ui/features/ShowDetails.js
+++ src/ui/features/ShowDetails.js
@@ -2,35 +2,17 @@ import { useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import { isEmpty } from "lodash";
 
-const ShowDetails = ({ isLoading, users, getUsers }) => {
-  useEffect(() => {
-    if (users.length === 0 && !isLoading) getUsers();
-  }, [users, getUsers]);
+const ShowDetails = (props) => {
 
   const { id } = useParams();
-  const user = users?.[id];
+
 
   //debugger;
 
   return (
     <div>
-      <h1>Bookmarkable Details Page</h1>
-      {!isLoading && (
-        <>
-          <div className="simple-row">
-            <span>{user?.name}</span>
-            <span>{user?.username}</span>
-            <span>{user?.email}</span>
-            <span>{user?.address.city}</span>
-            <span>{user?.address.zipcode}</span>
-          </div>
-        </>
-      )}
-      {isLoading && (
-        <>
-          <h1>LOADING...</h1>
-        </>
-      )}
+      <h1>Show details for show id {id}</h1>
+      
     </div>
   );
 };
